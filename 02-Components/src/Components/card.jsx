b1e06707127e14bf2cf0482f58cfa675205bde80
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-08-11 06:41:05 +0545

    Components in React

---
+++ 02-Components/src/Components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+    return (
+        <div>
+            <div className="card">
+                <h1>Rahul Singh</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, qui!</p>
+            </div>
+        </div>
+    )
+}
+
+export default card
