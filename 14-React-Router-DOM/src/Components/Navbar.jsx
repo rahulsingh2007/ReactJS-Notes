44083f64fcf8cfb599b26e27ae0c8cfae46978b1
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-08-26 06:57:13 +0545

    React Routing

---
+++ 14-React-Router-DOM/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="nav">
+                <h3>Coding</h3>
+                <div>
+                    <Link to='/'>Home</Link>
+                    <Link to='/About'>About</Link>
+                    <Link to='/Contact'>Contact</Link>
+                    <Link to='/Product'>Product</Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
