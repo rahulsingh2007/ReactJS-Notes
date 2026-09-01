8a7cdc67b3bb55bc420e73e9009ed22562071a88
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-09-01 06:59:24 +0545

    UseRef solved

--- 17-UseRef/src/App.jsx
+++ 17-UseRef/src/App.jsx
@@ -28,27 +28,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-/* import { useState, useEffect, useRef } from "react";
-
-const App = () => {
-  const [count, setCount] = useState(0);
-  const a = useRef(0); 
-
-  useEffect(() => {
-    a.current = a.current + 1;
-    console.log(`Rerendering and the value of a is ${a.current}`); 
-  });
-
-  return (
-    <div>
-      <h1>Count = {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Change Count</button>
-    </div>
-  );
-};
-
-export default App;
- */
